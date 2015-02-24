40fa53f5207512c234d0939952e94da6666d53cb
Erik Melander <erik@helgo.net>
2015-02-24 16:08:06 +0100

    now using IDSearchTree

--- breadthfirstsearch.hs
+++ breadthfirstsearch.hs
@@ -1,42 +1,96 @@
 import SteamAPI
 import FifoQueue
+import IDSearchTree
 
 test :: IO Integer
 test = return(76561198028357851)
-  
-main = do
-  let q = queue (test,[]) Empty
-  breadthFirstSearch q test []
 
+--Check if goal reached
+test1 = do
+  testid <- test
+  let q = queue (testid,[]) EmptyQ
+  breadthFirstSearch q 1 Empty
 
-breadthFirstSearch :: SimpleQueue ((IO Integer,[Integer])) -> IO Integer -> [Integer] -> IO ()
+--Check if queue empty
+test2 = do
+  testid <- test
+  let q = queue (testid,[]) EmptyQ
+  breadthFirstSearch EmptyQ 1 Empty
+
+--Check other...
+test3 =  do
+  testid <- test
+  let q = queue (testid,[]) EmptyQ
+  breadthFirstSearch q 12 Empty
+
+--testing checkAndAdd
+test4 = checkAndAdd EmptyQ [] Empty [1,2,3]
+
+{-
+
+PURPOSE		: Loops through the queue and terminates if queue is empty or looked for id is found.
+PRE 		: 
+POST 		:
+EXAMPLES 	:
+SIDEEFFECTS     : Plenty
+-}
+breadthFirstSearch :: SimpleQueue ((Integer,[Integer])) -> Integer -> Tree Integer -> IO ()
 breadthFirstSearch q goal visited = do
   if (isQueueEmpty q) then do
-    return ()
+    putStrLn "Didnt find any match before queue empty"
     else do
       let (entry, queue) = dequeue q
           (steamid, route) = (fst entry, snd entry)
-      lookupId <- steamid
-      (current,aList) <- parseryparsery $ getFriendList lookupId
+      aList <- parseryparsery $ getFriendList steamid
       if not(checkGoalReached aList goal) then do
-        breadthFirstSearch_aux q goal visited aList
+        let (newQueue, visitedUpdated) = checkAndAdd q (steamid:route) visited aList
+        breadthFirstSearch newQueue goal visitedUpdated
         else do
-        return()
+        putStrLn "I found it!!"
 
+{-
 
-parseryparsery :: IO String -> IO (IO Integer, [IO Integer])
-parseryparsery x = return (test, [test,test,test])
+PURPOSE		: Checks if id has already been visited or adds it to the queue unless the termination depth has been reached.
+PRE 		: 
+POST 		:
+EXAMPLES 	:		
+-}
+checkAndAdd :: SimpleQueue ((Integer, [Integer])) -> [Integer] -> Tree Integer  -> [Integer] -> (SimpleQueue ((Integer, [Integer])), Tree Integer)
+checkAndAdd q route prevVisited [] = (q,prevVisited)
+checkAndAdd q route  prevVisited (id:remainingId)
+  | not isVisited && not(checkDepthReached route) = checkAndAdd (queue (id,route) q) route visitedUpdated remainingId
+  | otherwise = checkAndAdd q route visitedUpdated remainingId
+    where
+      (visitedUpdated, isVisited) = searchTuple prevVisited id
+{-
 
+PURPOSE		: Placeholder function to fake JSON parser output.
+PRE 		: 
+POST 		:
+EXAMPLES 	:		
+-}
+parseryparsery :: IO String -> IO [Integer]
+parseryparsery x = return ([76561197989194839,76561198000124224,76561198043343260])
 
+{-
+
+PURPOSE		: Check if the looked for id has been found.
+PRE 		: 
+POST 		:
+EXAMPLES 	:		
+-}
 checkGoalReached [] _ = False
 checkGoalReached (x:xs) goal
   | x == goal = True
   | otherwise = checkGoalReached xs goal
 
+{-
 
+PURPOSE		: Checks if certain depth has been reached by the graph algorithm.
+PRE 		: 
+POST 		:
+EXAMPLES 	:		
+-}
 checkDepthReached :: [Integer] -> Bool
 checkDepthReached list = if (length list + 1) < 5 then False else True
 
-breadthFirstSearch_aux :: SimpleQueue ((IO Integer, [Integer])) -> IO Integer -> [Integer] ->[IO Integer] -> IO ()
-breadthFirstSearch_aux q goal visited toVisit = undefined
-  
--- fifoqueue.hs
+++ fifoqueue.hs
@@ -4,15 +4,15 @@ module FifoQueue where
 Two list fifo queue concept found at http://www.westpoint.edu/eecs/SiteAssets/SitePages/Faculty%20Publication%20Documents/Okasaki/jfp95queue.pdf
 -}            
 
-data SimpleQueue a = Empty | SQ [a] [a] deriving Show
+data SimpleQueue a = EmptyQ | SQ [a] [a] deriving Show
 
 dequeue :: SimpleQueue a -> (a, SimpleQueue a)
-dequeue (SQ (x:[]) []) = (x, Empty)
+dequeue (SQ (x:[]) []) = (x, EmptyQ)
 dequeue q@(SQ [] rearList) = dequeue (shiftQueue q)
 dequeue (SQ (x:frontList) rearList) = (x, (SQ frontList rearList))
 
 queue :: a -> SimpleQueue a -> SimpleQueue a
-queue x Empty = (SQ [x] [])
+queue x EmptyQ = (SQ [x] [])
 queue x (SQ frontList rearList) = (SQ frontList (x:rearList))
 
 shiftQueue :: SimpleQueue a -> SimpleQueue a
@@ -20,6 +20,6 @@ shiftQueue (SQ [] rearlist) = (SQ (reverse rearlist) [])
 shiftQueue q = q
 
 isQueueEmpty :: SimpleQueue a -> Bool
-isQueueEmpty Empty = True
+isQueueEmpty EmptyQ = True
 isQueueEmpty _ = False
 
